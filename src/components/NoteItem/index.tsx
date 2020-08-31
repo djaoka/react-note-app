8c4465650906d3958a120d4fe3b8346fe4527560
Johann laqua <johann@laqua.fr>
2020-09-01 00:16:35 +0200

    first basic elements

---
+++ src/components/NoteItem/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NoteItem extends Component {
+    render() {
+        return <div>the note</div>
+    }
+}
+
+export default NoteItem;
---
+++ src/components/NoteList/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import NoteItem from 'components/NoteItem';
+
+const NoteList = () => {
+    return (
+      <div>
+        <NoteItem />
+        <NoteItem />
+      </div>
+    );
+  }
+
+export default NoteList;
