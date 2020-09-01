76fe4250060a3a2137f9321bcca840c43d24af91
Johann laqua <johann@laqua.fr>
2020-09-02 00:13:58 +0200

    add some styles

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -5,13 +5,18 @@ import CSS from 'csstype';
 const styleContainer: CSS.Properties = {
     display: 'flex',
     flexDirection: 'column',
+    flexGrow: 1,
+    flexShrink: 0
 }
 
 const styleTitle: CSS.Properties = {
     fontWeight: 'bold',
     fontSize: '20px',
+    padding: '10px 20px',
+    borderBottom: '1px solid #ccc',
 }
 const styleText: CSS.Properties = {
+    padding: '10px 20px'
 }
 
 class NoteItem extends Component<NoteModel, {}> {
--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { NoteModel } from 'models/NoteModel';
 import { Link } from 'react-router-dom';
+import CSS from 'csstype';
 
 type NoteListProps = {
   notes: NoteModel[]
 }
 
+const styleList: CSS.Properties = {
+  listStyleType: 'none'
+}
+
 const NoteList = ({ notes }: NoteListProps) => {
     return (
-      <ul>
+      <ul style={styleList}>
         {notes.map((note: NoteModel, index) => (
           <Link key={index} to={`/${note.id}`}>
             <li>{note.title}</li>
