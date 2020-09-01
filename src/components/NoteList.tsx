6cf03dfa6191db81f0f91702a4584e14e20d1d45
Johann laqua <johann@laqua.fr>
2020-09-01 23:51:30 +0200

    add link to note

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NoteModel } from 'models/NoteModel';
+import { Link } from 'react-router-dom';
 
 type NoteListProps = {
   notes: NoteModel[]
@@ -9,7 +10,9 @@ const NoteList = ({ notes }: NoteListProps) => {
     return (
       <ul>
         {notes.map((note: NoteModel, index) => (
-          <li key={index}>{note.title}</li>
+          <Link key={index} to={`/${note.id}`}>
+            <li>{note.title}</li>
+          </Link>
         ))}
       </ul>
     );
