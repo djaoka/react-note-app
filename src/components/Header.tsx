491f252423f07cce15b7f6d43189eabb4879cab1
Johann laqua <johann@laqua.fr>
2020-09-01 18:27:45 +0200

    add some styles to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import CSS from 'csstype';
+
+const style: CSS.Properties = {
+    padding: '10px 40px',
+    backgroundColor: 'lightgrey',
+}
 
 class Header extends Component {
     render() {
-        return <header>My header</header>
+        return <header style={style}>
+            <button>New note</button>
+        </header>
     }
 }
 
